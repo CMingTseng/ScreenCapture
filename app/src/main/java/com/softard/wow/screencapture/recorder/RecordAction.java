13cd956064d77952ba03159ea99ffca8cf0fef21
neo <c.min.tseng@gmail.com>
2019-11-14 18:02:28 +0800

    export Record Action

---
+++ app/src/main/java/com/softard/wow/screencapture/recorder/RecordAction.java
@@ -0,0 +1,12 @@
+package com.softard.wow.screencapture.recorder;
+
+public class RecordAction {
+    public static final int MSG_PREPARE = 0;
+    public static final int MSG_START = 1;
+    public static final int MSG_FEED_INPUT = 2;
+    public static final int MSG_DRAIN_OUTPUT = 3;
+    public static final int MSG_RELEASE_OUTPUT = 4;
+    public static final int MSG_STOP = 5;
+    public static final int MSG_RELEASE = 6;
+    public static final int MSG_ERROR = -1;
+}
