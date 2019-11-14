de1ea863286b7406d42c1dd5f4eb19b3090eaaaa
Neo <c.min.tseng@gmail.com>
2019-11-14 16:27:42 +0800

    add EncodeConfig

---
+++ app/src/main/java/com/softard/wow/screencapture/config/EncodeConfig.java
@@ -0,0 +1,17 @@
+package com.softard.wow.screencapture.config;
+
+import java.util.Objects;
+
+public class EncodeConfig {
+    public String mCodecName;
+    public String mMIMEType;
+
+    /**
+     * @param codecName    selected codec name, maybe null
+     * @param codeMIMEType video MIME type, cannot be null
+     */
+    public EncodeConfig(String codecName, String codeMIMEType) {
+        mCodecName = codecName;
+        mMIMEType = Objects.requireNonNull(codeMIMEType);
+    }
+}
