90f21519d177e715063dd72278e5c5efe91b8985
neo <c.min.tseng@gmail.com>
2019-11-15 18:54:50 +0800

    add InfDef annotation

--- app/src/main/java/com/softard/wow/screencapture/recorder/RecordAction.java
+++ app/src/main/java/com/softard/wow/screencapture/recorder/RecordAction.java
@@ -1,5 +1,10 @@
 package com.softard.wow.screencapture.recorder;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import androidx.annotation.IntDef;
+
 public class RecordAction {
     public static final int MSG_PREPARE = 0;
     public static final int MSG_START = 1;
@@ -9,4 +14,11 @@ public class RecordAction {
     public static final int MSG_STOP = 5;
     public static final int MSG_RELEASE = 6;
     public static final int MSG_ERROR = -1;
+
+    @IntDef(value = {
+            MSG_PREPARE, MSG_START, MSG_FEED_INPUT, MSG_DRAIN_OUTPUT, MSG_RELEASE_OUTPUT, MSG_STOP, MSG_RELEASE, MSG_ERROR
+    })
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface Action {
+    }
 }
